f4455cd88abfd274e9529babd44fa9dd73cdfef6
Douglas Wu <douglas@vant.ai>
2024-10-04 15:26:36 +0000

    intial add files

---
+++ parquet.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import click
+import polars as pl
+
+
+@click.command(help="A tool to sneak peek parquet files")
+@click.option("-p", "--parquet-fn", help="Parquet file to peek", type=str, required=True)
+@click.option("-l", "--lines-to-show", help="how many lines to show", type=int, default = 10)
+@click.option(
+    "-w",
+    "--width",
+    help="char width to print in each " "columns of the data frame",
+    type=int,
+    default=250
+)
+def main(parquet_fn, lines_to_show, width):
+    df = pl.scan_parquet(parquet_fn)
+    with pl.Config(fmt_str_lengths=width, tbl_width_chars=width, tbl_cols=-1):
+        print(df.head(lines_to_show).collect())
+
+
+if __name__ == "__main__":
+    main()
