751cc1ec20e794dfa6c07c283f4e18b60f646876
wckdouglas <douglas@vant.ai>
2024-10-07 10:46:21 -0400

    adding toolings

--- src/parquet_peek/__init__.py
+++ src/parquet_peek/__init__.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
-
-import rich_click as click
 import polars as pl
+import rich_click as click
 
 
 @click.command(help="A tool to sneak peek parquet files")
-@click.option("-p", "--parquet-fn", help="Parquet file to peek", type=str, required=True)
-@click.option("-l", "--lines-to-show", help="how many lines to show", type=int, default = 10)
+@click.option(
+    "-p", "--parquet-fn", help="Parquet file to peek", type=str, required=True
+)
+@click.option(
+    "-l", "--lines-to-show", help="how many lines to show", type=int, default=10
+)
 @click.option(
     "-w",
     "--width",
     help="char width to print in each " "columns of the data frame",
     type=int,
-    default=250
+    default=250,
 )
 def main(parquet_fn, lines_to_show, width):
     df = pl.scan_parquet(parquet_fn)
