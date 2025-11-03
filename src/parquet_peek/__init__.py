b96f906c38e8a2f741a9dd6126ee325a5b570cae
wckdouglas <douglas@vant.ai>
2025-11-03 12:28:50 -0500

    adding a check

--- src/parquet_peek/__init__.py
+++ src/parquet_peek/__init__.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python
 import polars as pl
 import rich_click as click
+from google.cloud import storage
+
+
+def extract_bucket(gcs_dir: str):
+    if not gcs_dir.startswith("gs://"):
+        raise ValueError("GCS path must start with 'gs://'")
+    split_fields = gcs_dir.split("/")
+    bucket_name = split_fields[2]
+    blob_name = "/".join(split_fields[3:])
+    return bucket_name, blob_name
+
+
+def check_file_existence(gcs_file_name: str) -> bool:
+    """Checks if a file exists in a GCS bucket."""
+    bucket_name, blob_name = extract_bucket(gcs_file_name)
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    return blob.exists()
 
 
 @click.command(help="A tool to sneak peek parquet files")
@@ -18,6 +37,8 @@ import rich_click as click
     default=250,
 )
 def main(parquet_fn, lines_to_show, width):
+    if not check_file_existence(parquet_fn):
+        raise FileNotFoundError(f"File {parquet_fn} not found in GCS.")
     df = pl.scan_parquet(parquet_fn)
     with pl.Config(fmt_str_lengths=width, tbl_width_chars=width, tbl_cols=-1):
         print(df.head(lines_to_show).collect())
